e974cec26af3ff312e9514450aed39914d15fc27
jromerooo2 <75763058+jromerooo2@users.noreply.github.com>
2021-03-27 00:58:58 -0600

    LogIn with local Storage, done

--- assets/localStorage.js
+++ assets/localStorage.js
@@ -1,22 +1,29 @@
 
     const save = e =>{
-        let DataFromForms = {
-           UserName: document.getElementById("username").value,
-           Password: document.getElementById("password").value
-        }
+        let username = document.getElementById("username").value;
+       let password =  document.getElementById("password").value
 
-        localStorage.setItem("DataFromForms", JSON.stringify(DataFromForms));
-        console.log(localStorage.getItem("DataFromForms"));
+         localStorage.setItem("username", JSON.stringify(username));
+        localStorage.setItem("password", JSON.stringify(password));
+        console.log(localStorage.getItem("username"));
         e.preventDefault();
 
     }
 
     function LogIn(){
-        let log = false;
-        alert("uiii");
+        let usernameLog = document.getElementById("username").value;
+        let passwordLog = document.getElementById("username").value;
+        let username = localStorage.getItem("username");
+        let password = localStorage.getItem("password");
 
-        if(log == true ){
+        if(usernameLog == username && passwordLog == password){
             window.location.replace("http://www.w3schools.com");
+        }
+        else{
+            let sumbi = document.getElementById("message");
+            let nav = document.getElementById("nav");
+            nav.style.backgroundColor = "red";
+            submi.innerHTML = "OOPS...<br> Parece que tus credenciales son incorrectas!";
         }
         
     }
