1cef0a5b6710b83a5403b8c8c798fcccc841a9c3
jromerooo2 <75763058+jromerooo2@users.noreply.github.com>
2021-04-06 10:30:24 -0600

    Initial commit secondary parts
    secondary parts

--- assets/localStorage.js
+++ assets/localStorage.js
@@ -1,29 +1,52 @@
 
-    const save = e =>{
-        let username = document.getElementById("username").value;
-       let password =  document.getElementById("password").value
+    // const save = e =>{
+    //     let username = document.getElementById("username").value;
+    //    let password =  document.getElementById("password").value
 
-         localStorage.setItem("username", JSON.stringify(username));
-        localStorage.setItem("password", JSON.stringify(password));
-        console.log(localStorage.getItem("username"));
-        e.preventDefault();
+    //      localStorage.setItem("username", JSON.stringify(username));
+    //     localStorage.setItem("password", JSON.stringify(password));
+    //     console.log(localStorage.getItem("username"));
+    //     console.log(localStorage.getItem("password"));
+    //     e.preventDefault();
+
+    //     let usernameLog = document.getElementById("username").value;
+    //     let passwordLog = document.getElementById("username").value;
+    //     let username = localStorage.getItem("username").value;
+    //     let password = localStorage.getItem("password");
+
+    //     console.log(localStorage.getItem("username"));
+    //     console.log(localStorage.getItem("password"));
+
+    //     if(usernameLog == username ){
+    //         window.location.replace("http://www.w3schools.com");
+    //     }
+    //     else{
+    //         let sumbi = document.getElementById("message");
+    //         let nav = document.getElementById("nav");
+    //         nav.style.backgroundColor = "red";
+    //         submi.innerHTML = "OOPS...<br> Parece que tus credenciales son incorrectas!";
+  
+    function logIn(){
+        if(localStorage){
+            //alert("hi")
+            let username = document.getElementById("username").value;
+            let password =  document.getElementById("password").value;
+    
+            let user = localStorage.setItem("username", JSON.stringify(username));
+            let pass = localStorage.setItem("password", JSON.stringify(password));
+    
+            console.log(localStorage.getItem("username"));
+            console.log(localStorage.getItem("password"));
+            
+            if(user == username){
+                alert("hi");
+            }
+    
+        }
+
+
 
     }
 
-    function LogIn(){
-        let usernameLog = document.getElementById("username").value;
-        let passwordLog = document.getElementById("username").value;
-        let username = localStorage.getItem("username");
-        let password = localStorage.getItem("password");
-
-        if(usernameLog == username && passwordLog == password){
-            window.location.replace("http://www.w3schools.com");
-        }
-        else{
-            let sumbi = document.getElementById("message");
-            let nav = document.getElementById("nav");
-            nav.style.backgroundColor = "red";
-            submi.innerHTML = "OOPS...<br> Parece que tus credenciales son incorrectas!";
-        }
         
-    }
+
