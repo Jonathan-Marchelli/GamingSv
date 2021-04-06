aa6d22cfb523f6e72833d0135fd12d23a17d9788
jromerooo2 <75763058+jromerooo2@users.noreply.github.com>
2021-04-06 10:53:44 -0600

    Main page finished

--- assets/localStorage.js
+++ assets/localStorage.js
@@ -1,30 +1,3 @@
-
-    // const save = e =>{
-    //     let username = document.getElementById("username").value;
-    //    let password =  document.getElementById("password").value
-
-    //      localStorage.setItem("username", JSON.stringify(username));
-    //     localStorage.setItem("password", JSON.stringify(password));
-    //     console.log(localStorage.getItem("username"));
-    //     console.log(localStorage.getItem("password"));
-    //     e.preventDefault();
-
-    //     let usernameLog = document.getElementById("username").value;
-    //     let passwordLog = document.getElementById("username").value;
-    //     let username = localStorage.getItem("username").value;
-    //     let password = localStorage.getItem("password");
-
-    //     console.log(localStorage.getItem("username"));
-    //     console.log(localStorage.getItem("password"));
-
-    //     if(usernameLog == username ){
-    //         window.location.replace("http://www.w3schools.com");
-    //     }
-    //     else{
-    //         let sumbi = document.getElementById("message");
-    //         let nav = document.getElementById("nav");
-    //         nav.style.backgroundColor = "red";
-    //         submi.innerHTML = "OOPS...<br> Parece que tus credenciales son incorrectas!";
   
     function logIn(){
         if(localStorage){
