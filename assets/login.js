007fece9c8825da97313b0af3292c724b78c462f
jromerooo2 <75763058+jromerooo2@users.noreply.github.com>
2021-04-07 16:45:03 -0600

    ok

--- assets/login.js
+++ assets/login.js
@@ -3,13 +3,14 @@
  const username = document.getElementById("user");
  const password =  document.getElementById("pass");
  let navigator = document.getElementById('message');
+ let navD = document.getElementById('nav');
 
     //Comrpobando info
     formLogin.addEventListener('submit', (e)=>{
+
         e.preventDefault();
-
+        
         let userStorage = JSON.parse(localStorage.getItem('usuario'));
-
 
         let usuario = username.value;
         let contra = password.value;
@@ -22,13 +23,15 @@
             }
             else{
                 console.log('no son iguales')
-                navigator.innerHTML = "OOPS... parece que los datos son erroneos, revisa que todo este bien!"
+                navigator.innerHTML = "OOPS... parece que los datos son erroneos, revisa que todo este bien!";
+                navD.style.backgroundColor = "red";
             }
             
         }
         else{
             console.log('no account')
             navigator.innerHTML = "OOPS... parece que no tienes cuenta aun, crea una y luego inicia sesion!"
+            navD.style.backgroundColor = "red";
         }
 
 
